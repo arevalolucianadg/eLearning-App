ac455b1d7f88cbec7a784ebece2cde3d888436b0
Luciana Arevalo <arevalolucianadg@gmail.com>
2021-02-27 20:35:06 -0300

    Add config Eslint

--- src/api/app.js
+++ src/api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const morgan  = require('morgan');
+const morgan = require('morgan');
 
 const app = express();
-app.use( morgan('dev') );
+app.use(morgan('dev'));
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -2,5 +2,4 @@ const app = require('./api/app');
 
 const PORT = process.env.PORT || 8080;
 
-
-const server = app.listen(PORT, () => console.log('Server listening on port', PORT));
+app.listen(PORT, () => console.log('Server listening on port', PORT));
